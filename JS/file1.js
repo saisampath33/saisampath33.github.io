ee357268b784685aa127169a3396a5e0dcbf2f8c
saisampath33 <sampathmay10@gmail.com>
2025-06-20 09:58:38 +0530

    updated js

---
+++ JS/file1.js
@@ -0,0 +1,22 @@
+console.log("Hello World")
+//variables and types
+let sname = "Sai"
+let age = 21
+let promoted = false
+let score //type is undefined
+let city = null
+// console.log(sname)
+// console.log(typeof sname)
+// console.log( typeof city)
+
+let strAge = String(age) //typecasting of age variable from number to string
+console.log( typeof strAge)
+
+let rating = "5"
+console.log(typeof rating)
+let strrating = Number(rating); //conversion from string to number
+console.log(typeof strrating)
+
+let flag = "true"
+let boolflag = Boolean(flag)
+console.log(typeof boolflag)
---
+++ JS/file2.js
@@ -0,0 +1,23 @@
+//OPERATORS
+
+// let a =10
+// let b=20
+// console.log(a+b) //addition
+// console.log(a-b) //subtraction
+// console.log(a*b) //multiplication
+// console.log(a/b) //division
+// console.log(a**b) //exponentiation
+// console.log(a%b) //modulus
+
+let a = "10"
+let b= 10
+// console.log(a+b) //concatenation 1010
+// console.log(a*b) //multiplication 100
+
+// console.log(a==b) //loose equality, checks value only, true
+// console.log(a===b) //strict equality, checks value and type, false
+// console.log(a!=b) //loose inequality, checks value only, false
+// console.log(a!==b) //strict inequality, checks value and type, true
+
+// console.log(a<b) //less than, false
+console.log(!(a<b)) //not less than, true

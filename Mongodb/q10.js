27838935a15db30b792919ab1c3599c2dc4ead9c
saisampath33 <sampathmay10@gmail.com>
2025-07-01 11:01:34 +0530

    updated

--- Mongodb/q10.js
+++ Mongodb/q10.js
@@ -1,18 +1,32 @@
-// Create a folder mongo-replica and sub folders data1 data2 and data3
+// // Create a folder mongo-replica and sub folders data1 data2 and data3
 
-// Open command prompt and start running servers on separate tabs
+// // Open command prompt and start running servers on separate tabs
 
-mongod -replSet rs1 -logpath "S:\mongo-replica\data1\1.log" --dbpath "S:\mongo-replica\data1" --port 27018
+// mongod -replSet rs1 -logpath "S:\mongo-replica\data1\1.log" --dbpath "S:\mongo-replica\data1" --port 27018
 
-mongod -replSet rs1 -logpath "S:\mongo-replica\data2\2.log" --dbpath "S:\mongo-replica\data2" --port 27019
+// mongod -replSet rs1 -logpath "S:\mongo-replica\data2\2.log" --dbpath "S:\mongo-replica\data2" --port 27019
 
-mongod -replSet rs1 -logpath "S:\mongo-replica\data3\3.log" --dbpath "S:\mongo-replica\data3" --port 27020
+// mongod -replSet rs1 -logpath "S:\mongo-replica\data3\3.log" --dbpath "S:\mongo-replica\data3" --port 27020
 
-Follow these instructions to configure replica set:
+// Follow these instructions to configure replica set:
 
-mongosh --port 27018
+// mongosh --port 27018
 
-rs.initiate({_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]})
+// rs.initiate({_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]})
 
-rs.config()  //to check the config
-rs.status()
+// rs.config()  //to check the config
+// rs.status()
+
+// Use mongosh command with the following connection string and the primary server will automatically get connected:
+
+
+// mongosh "mongodb://localhost:27018,localhost:27019,localhost:27020/?replicaSet=rs1"
+
+// show dbs
+// use mytestdb
+// db.createCollection("customers")
+// db.customers.insertOne({name:"John"})
+
+// db.getMongo().setReadPref("secondary")
+
+// //real scenairio ip address will differ
